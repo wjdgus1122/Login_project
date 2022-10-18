9b81aaf1086f4c53d914227877a4528f8e39a9ab
wjdgus1122 <qodqodantk@gmail.com>
2022-10-18 23:20:49 +0900

    home design

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,56 @@
+import styled from "styled-components";
+
+const HomeWrap = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Con = styled.div`
+  width: 30%;
+  height: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Text = styled.div`
+  font-size: 50px;
+`;
+const BtnWrap = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Btn = styled.div`
+  width: 30%;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 25px;
+  cursor: pointer;
+  transition: 0.5s;
+  &:hover {
+    background-color: beige;
+    font-style: oblique;
+    color: salmon;
+  }
+`;
+
 export const Home = () => {
-  return <>HOME!!</>;
+  return (
+    <HomeWrap>
+      <Con>
+        <Text>Home!</Text>
+        <BtnWrap>
+          <Btn link={"/signup"}>SignUp</Btn>
+          <Btn link={"/login"}>Login</Btn>
+        </BtnWrap>
+      </Con>
+    </HomeWrap>
+  );
 };
